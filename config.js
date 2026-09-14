3ab04cc72b7715014cd4f658778b00b3ea6b0d87
The24KFool <brad.allison@gmx.us>
2026-09-13 22:05:08 -0400

    first publish

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const SUPABASE_URL = "https://iftbqnomobkspinocjdm.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_xA4p8jNp8yxZy367fE2UUQ_mz2ULFzt";
+
+// Initialize Supabase Client
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
